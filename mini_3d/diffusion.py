ee7e947b79c3cfa645498169c5c381681036a1fe
Eric Chen <eric.chen@backflip.ai>
2024-04-30 05:31:55 +0000

    forward diffusion works

---
+++ mini_3d/diffusion.py
@@ -0,0 +1,92 @@
+from typing import Literal
+
+import torch
+import torch.nn as nn
+
+from .dit import DiT
+
+
+# noise schedule
+def linear_beta_schedule(num_timesteps: int):
+    scale = 1000 / num_timesteps
+    beta_start = scale * 1e-4
+    beta_end = scale * 0.02
+
+    return torch.linspace(beta_start, beta_end, num_timesteps)
+
+
+def cosine_beta_schedule(num_timesteps: int):
+    s = 0.008
+    t = torch.linspace(0, num_timesteps, num_timesteps + 1)
+    f = torch.cos((t / num_timesteps + s) / (1 + s) * (torch.pi / 2)) ** 2
+    alpha_cumprod = f / f[0]
+    beta = 1 - (alpha_cumprod[1:] / alpha_cumprod[:-1])
+    beta = torch.clamp(beta, 0, 0.999)
+
+    return beta
+
+
+def extract(arr: torch.Tensor, t: torch.Tensor, shape: torch.Size):
+    """Extract value from arr at timestep t and make it broadcastable to shape."""
+    B = t.shape[0]
+    val = arr[t]
+    return val.reshape(B, *(1 for _ in range(len(shape) - 1)))
+
+
+class Diffusion(nn.Module):
+    """
+    Utilities for noising and denoising during the gaussian diffusion process
+    """
+
+    def __init__(
+        self,
+        model: DiT,
+        schedule_type: Literal["linear", "cosine"] = "linear",
+        num_timesteps: int = 1000,
+        learn_sigma: bool = False,
+    ):
+        super().__init__()
+        self.model = model
+        self.schedule_type = schedule_type
+        self.num_timesteps = num_timesteps
+        self.learn_sigma = learn_sigma
+
+        # create noise schedule and values we'll use during forward and reverse diffusion
+        if schedule_type == "linear":
+            beta = linear_beta_schedule(num_timesteps)
+        elif schedule_type == "cosine":
+            beta = cosine_beta_schedule(num_timesteps)
+
+        alpha = 1 - beta
+        alpha_cumprod = torch.cumprod(alpha, dim=-1)
+        sqrt_alpha_cumprod = torch.sqrt(alpha_cumprod)
+        sqrt_one_minus_alpha_cumprod = torch.sqrt(1 - alpha_cumprod)
+
+        # register the values as buffers so we can move them to any device easily
+        self.register_buffer("beta", beta)
+        self.register_buffer("alpha", alpha)
+        self.register_buffer("alpha_cumprod", alpha_cumprod)
+        self.register_buffer("sqrt_alpha_cumprod", sqrt_alpha_cumprod)
+        self.register_buffer(
+            "sqrt_one_minus_alpha_cumprod", sqrt_one_minus_alpha_cumprod
+        )
+
+    def q_sample(
+        self, x_start: torch.Tensor, t: torch.Tensor, noise: torch.Tensor = None
+    ):
+        """
+        Adds noise for t timesteps to x_start to get noised sample x_t.
+        Equivalent to x_t ~ q(x_t | x_start).
+        """
+        if noise is None:
+            noise = torch.randn_like(x_start)
+
+        x_t = (
+            extract(self.alpha_cumprod, t, x_start.shape) * x_start
+            + extract(self.sqrt_one_minus_alpha_cumprod, t, x_start.shape) * noise
+        )
+
+        return x_t
+
+    def forward(self):
+        pass
--- mini_3d/utils.py
+++ mini_3d/utils.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torch.nn as nn
 import trimesh
 from mpl_toolkits.mplot3d import Axes3D
+from PIL import Image
+from torchvision.transforms import v2
 
 
 def visualize_point_cloud(point_cloud: torch.Tensor):
@@ -25,3 +28,29 @@ def convert_point_cloud_to_ply(point_cloud: torch.Tensor, save_path: str):
 
     point_cloud = trimesh.points.PointCloud(point_cloud)
     point_cloud.export(save_path)
+
+
+def to_pil_image(x: torch.Tensor):
+    # normalize between 0 and 1
+    x = ((x + 1) / 2).clamp(0, 1)
+    return v2.ToPILImage()(x)
+
+
+def plot_images(images: list[Image.Image], **kwargs):
+    rows = 1
+    cols = len(images)
+
+    fig, axs = plt.subplots(figsize=(25, 25), nrows=rows, ncols=cols, squeeze=True)
+
+    if len(images) == 1:
+        axs = [axs]
+
+    for i, img in enumerate(images):
+        axs[i].imshow(img, **kwargs)
+        axs[i].axis("off")
+
+    plt.tight_layout()
+
+
+def count_parameters(model: nn.Module):
+    return sum(p.numel() for p in model.parameters())
