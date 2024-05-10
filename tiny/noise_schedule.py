041768e1e129d951cb112d642ab8386bbb540045
Eric Chen <eric.chen@backflip.ai>
2024-05-10 00:16:41 +0000

    template for noise scheduler

--- tiny/noise_schedule.py
+++ tiny/noise_schedule.py
@@ -1,5 +1,18 @@
+from typing import Literal
+
+import torch
+import torch.nn as nn
+
+ScheduleType = Literal["linear", "cosine"]
 
 
+class NoiseScheduler(nn.Module):
+    def __init__(self, num_timesteps: int, schedule_type: ScheduleType = "cosine"):
+        super().__init__()
 
-class NoiseScheduler:
-    pass
+    def add_noise(
+        self, x_start: torch.Tensor, t: torch.Tensor, noise: torch.Tensor = None
+    ):
+        """
+        Adds noise to x_start for t timesteps. Samples x_t ~ q(x_t | x_start).
+        """
