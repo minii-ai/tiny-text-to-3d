6d97cbd7f4ab67294862d02f21fb855f101f14f9
Eric Chen <eric.chen@backflip.ai>
2024-05-10 00:23:43 +0000

    template for diffusion

--- tiny/ddpm.py
+++ tiny/ddpm.py
@@ -1,2 +1,34 @@
-class DDPM:
-    pass
+import torch
+import torch.nn as nn
+
+from .dit import PointCloudDiT
+from .noise_schedule import NoiseScheduler
+from .sampler import Sampler
+
+
+class Diffusion(nn.Module):
+    def __init__(
+        self,
+        noise_scheduler: NoiseScheduler,
+        model: PointCloudDiT,
+        sampler: Sampler,
+        shape: tuple,
+    ):
+        super().__init__()
+
+    def loss(self, x_start: torch.Tensor):
+        pass
+
+    def sample_loop():
+        pass
+
+    def sample_loop_progressive():
+        pass
+
+    def forward(
+        self,
+        batch_size: int = 1,
+        num_inference_steps: int = 1000,
+        guidance_scale: float = 1.0,
+    ):
+        pass
