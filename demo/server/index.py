1b4f343ac4f9fd052140ef8d45667414af3a7d3a
Eric Chen <eric.chen@backflip.ai>
2024-05-19 17:57:02 +0000

    websocket server ready

--- demo/server/index.py
+++ demo/server/index.py
@@ -1,7 +1,30 @@
+import argparse
 import asyncio
 import json
+import os
+import sys
 
+import torch
 import websockets
+
+sys.path.append("../../")
+
+from tiny import PointCloudDiffusion
+from tiny.utils import plot_point_clouds
+
+store = {}
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--weights", type=str, default="../../weights/modelnet40")
+
+    return parser.parse_args()
+
+
+def read_json(path: str):
+    with open(path, "r") as f:
+        return json.load(f)
 
 
 async def handler(websocket, path):
@@ -9,14 +32,36 @@ async def handler(websocket, path):
         async for message in websocket:
             print(f"Received message: {message}")
             message = json.loads(message)
+            prompt = message["prompt"]
+            diffusion = store["diffusion"]
 
-            print(message)
-            await websocket.send(f"Echo: {message}")
+            for sample in diffusion.sample_loop_progressive(
+                1, [prompt], num_inference_steps=20, guidance_scale=2.0, use_cfg=True
+            ):
+                points = sample[0].tolist()
+                data = json.dumps({"points": points})
+                await websocket.send(data)
+
     except websockets.ConnectionClosedOK:
         print("Connection closed")
 
 
 async def main():
+    # load model
+    args = parse_args()
+    config_path = os.path.join(args.weights, "config.json")
+    weights_path = os.path.join(args.weights, "weights.pt")
+    config = read_json(config_path)
+    checkpoint = torch.load(weights_path)
+
+    if "weights" in checkpoint:
+        checkpoint = checkpoint["weights"]
+
+    diffusion = PointCloudDiffusion.from_config(config)
+    diffusion.model.load_state_dict(checkpoint)
+    store["diffusion"] = diffusion
+
+    # start server
     server = await websockets.serve(handler, "localhost", 8000)
     print("WebSocket server is running on ws://localhost:8000")
     await server.wait_closed()
